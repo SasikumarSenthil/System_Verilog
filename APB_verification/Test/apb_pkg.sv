c7b51dcba625fe39faeded07e2af6251bb1b9802
sasi <122765403+SasikumarSenthil@users.noreply.github.com>
2026-09-22 22:17:24 +0530

    Create apb_pkg package with necessary includes

---
+++ APB_verification/Test/apb_pkg.sv
@@ -0,0 +1,11 @@
+package apb_pkg;
+   event ev;
+`include "../env/apb_transaction.sv"
+`include "../env/apb_generator.sv"
+`include "../env/apb_driver.sv"
+`include "../env/apb_monitor.sv"
+`include "../env/apb_scoreboard.sv"
+`include "../env/apb_environment.sv"
+`include "../test/apb_test.sv"   
+
+endpackage: apb_pkg
